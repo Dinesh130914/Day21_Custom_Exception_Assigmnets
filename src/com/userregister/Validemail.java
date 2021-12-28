558de4e62e142eb78116d5c1c54ca62fc6ff512f
Dinesh130914 <dineshthiyagu99@gmail.com>
2021-12-28 16:08:42 +0530

    Added:Validate Email Id

---
+++ src/com/userregister/Validemail.java
@@ -0,0 +1,26 @@
+package com.userregister;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validemail {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter the emailid");
+		String email = scanner.next();
+		
+		String pattern = "^[abc]+.+[a-z]+@[bl]+.+[co]+.+[a-z]{2,5}$";
+		Pattern regex = Pattern.compile(pattern);
+		Matcher inputMatcher = regex.matcher(email);
+		
+		if(inputMatcher.matches())
+		{
+			System.out.println("Valid email id");
+		}else 
+		{
+			System.out.println("Invalid email id");
+		}
+	}
+}
